41b3fb1835666e7b2541b400912fc0cab23ad8fc
Mortezaahm <114913972+Mortezaahm@users.noreply.github.com>
2023-02-28 10:24:03 +0100

    Welcome page new

--- View/login.js
+++ View/login.js
@@ -1,15 +1,44 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // prevent the form from submitting
+$(document).ready(function() {
+    var loginUsername;
+    var loginPassword;
 
-    // get the username and password values
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    document.getElementById("sign").innerHTML = "Login"
+    document.getElementById("header").innerHTML = "Welcome to our ship";
+    document.getElementById("welcome").innerHTML = "Welcome On Board";
+    document.getElementById("sign-welcome").innerHTML = "Sign In";
+    document.getElementById("username").innerHTML = "Username :";
+    document.getElementById("password").innerHTML = "Password :";
+    document.getElementById("lBtn").innerHTML = "Login";
 
-    // check if the username and password are correct
-    if (username === 'user' && password === 'password') {
-        alert('Login successful!');
-    } else {
-        alert('Incorrect username or password.');
-    }
+    //Login form
+    document.getElementById("username").innerHTML = "Username :";
+    document.getElementById("password").innerHTML = "Password :";
+    document.getElementById("lBtn").innerHTML = "Login";
+
+    $('#lBtn').on('click', function() {
+        var loginUsernameEntry = $("#username1").val();
+        var loginPasswordEntry = $("#password1").val();
+        $.getJSON("../databases/VIP.json", function(data){
+            // console.log(data); // Prints: Harry
+            loginUsername = data.find(item => item.username === loginUsernameEntry);
+            loginPassword = data.find(item => item.password === loginPasswordEntry);
+
+            if (loginUsernameEntry === loginUsername?.username && loginPasswordEntry === loginPassword?.password) {
+
+                console.log("Current Username " + loginUsername.username);
+                console.log("Current Password " + loginPassword.password);
+                console.log("Logged In");
+            } else {
+                console.log("Attempted Username " + loginUsernameEntry);
+                console.log("Attempted Password " + loginPasswordEntry);
+
+                console.log("Login Falied");
+            };
+
+        }).fail(function(){
+            console.log("An error has occurred.");
+        });
+
+    });
+
 });
